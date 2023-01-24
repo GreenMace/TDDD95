d7a1387378811faa39f9ce49257a258d35ded5db
Magnus Hjortswang <maghj433@student.liu.se>
2023-01-24 10:39:49 +0100

    An attempt at a better solution for logincsubseq

--- Lab1/logincsubseq.cc
+++ Lab1/logincsubseq.cc
@@ -1,30 +1,49 @@
 #include <iostream>
 #include <vector>
-#include <deque>
-#include <unordered_map>
+#include <map>
 #include <algorithm>
 
-std::unordered_map<int, std::deque<int>> longestSequences;
-
-std::deque<int> solve(std::vector<int> sequence, int start = 0) {
-    std::deque<int> output = {start};
-    if (longestSequences.count(start)) {
-        return longestSequences[start];
+int getKey(std::map<int, std::vector<int>>& v, std::vector<int> sequence, int l, int r, int elem)
+{
+    while (r - l > 1) {
+        int m = l + (r - l) / 2;
+        if (sequence[v[m].back()] >= elem) {
+            r = m;
+        } else {
+            l = m;
+        }
     }
+  
+    return r;
+}
 
-    for (int i = start + 1; i < sequence.size(); i++) {
-        if (sequence[i] > sequence[start]) {
-            std::deque<int> seq = solve(sequence, i);
-            if (seq.size() >= output.size()) {
-                output = seq;
-                output.push_front(start);
+std::vector<int> solve(std::vector<int> sequence) {
+    std::map<int, std::vector<int>> activeLists;
+
+    activeLists[0] = {0};
+    for (int i = 1; i < sequence.size(); i++) {
+        int elem = sequence[i];
+
+        if (elem < sequence[activeLists[0].back()]) {
+            activeLists[0] = {i};
+        } else if (elem > sequence[activeLists.rbegin()->second.back()]) {
+            std::vector<int> seq =  activeLists.rbegin()->second;
+            seq.push_back(i);
+            activeLists[seq.size() - 1] = seq;
+        } else {
+            int largestSmallerKey = getKey(activeLists, sequence, -1, activeLists.size()-1, elem);
+
+            if (sequence[activeLists[largestSmallerKey].back()] == elem) {
+                largestSmallerKey -= 1;
             }
+
+            std::vector<int> seq = activeLists[largestSmallerKey];
+            seq.push_back(i);
+            activeLists[seq.size() - 1] = seq;
         }
     }
 
-    longestSequences[start] = output;
-
-    return output;
+    return activeLists.rbegin()->second;
 }
 
 int main(int argc, char const *argv[]) {
@@ -37,9 +56,9 @@ int main(int argc, char const *argv[]) {
             std::cin >> num;
             sequence.push_back(num);
         }
-        
-        longestSequences = {};
-        std::deque<int> longest = solve(sequence);
+
+        std::vector<int> longest = solve(sequence);
+
         std::cout << longest.size() << "\n";
         for (int i : longest) {
             std::cout << i << " ";
